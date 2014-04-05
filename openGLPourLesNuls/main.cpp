526b5cd8d749e853c165ab7a01da7b4ebc6e8a2f
aubert_n <aubert_n@epitech.eu>
2014-04-05 16:21:32 +0200

    green cube rotating OPENGL POWA

--- openGLPourLesNuls/main.cpp
+++ openGLPourLesNuls/main.cpp
@@ -4,51 +4,125 @@
 // Made by Nathan AUBERT
 // Login   <aubert_n@epitech.net>
 // 
-// Started on  Fri Apr  4 21:12:23 2014 Nathan AUBERT
-// Last update Sat Apr  5 16:00:33 2014 Nathan AUBERT
+// Started on  Sat Apr  5 16:17:40 2014 Nathan AUBERT
+// Last update Sat Apr  5 16:20:59 2014 Nathan AUBERT
 //
 
 #include <SFML/Window.hpp>
 #include <SFML/OpenGL.hpp>
+#include<SFML/Graphics.hpp>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <iostream>
 
+
+GLfloat     Rotate; 
+
+int width, height;
+   
+using namespace std;
 int main()
 {
-  // crée la fenêtre
-  sf::Window window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Default, sf::ContextSettings(32));
-  window.setVerticalSyncEnabled(true);
+// create the window
+sf::RenderWindow window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Default, sf::ContextSettings(32));
+window.setVerticalSyncEnabled(true);
+width = 256;
+height = 256;
+glClearDepth(1.f);
+glClearColor(0.f, 0.f, 0.f, 0.f);
+glMatrixMode(GL_PROJECTION);
+glDepthMask(GL_TRUE);
+glEnable(GL_DEPTH_TEST);
+// load resources, initialize the OpenGL states, ...
 
-  // chargement des ressources, initialisation des états OpenGL, ...
-
-  // la boucle principale
-  bool running = true;
-  while (running)
-    {
-  // gestion des évènements
-  sf::Event event;
-  while (window.pollEvent(event))
-    {
-  if (event.type == sf::Event::Closed)
-    {
-  // on stoppe le programme
-  running = false;
+GLuint Texture = 0;
+{
+sf::Image Image;
+if (!Image.loadFromFile("GreenCube.bmp"))
+  return EXIT_FAILURE;
+glGenTextures(1, &Texture);
+glBindTexture(GL_TEXTURE_2D, Texture);
+gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGBA, width, height, GL_RGBA, GL_UNSIGNED_BYTE, Image.getPixelsPtr());
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 }
-  else if (event.type == sf::Event::Resized)
-    {
-  // on ajuste le viewport lorsque la fenêtre est redimensionnée
-  glViewport(0, 0, event.size.width, event.size.height);
+// run the main loop
+
+bool running = true;
+while (running)
+  {
+// handle events
+sf::Event event;
+while (window.pollEvent(event))
+  {
+if (event.type == sf::Event::Closed)
+  {
+// end the program
+running = false;
+}
+ else if (event.type == sf::Event::Resized)
+   {
+// adjust the viewport when the window is resized
+glViewport(0, 0, event.size.width, event.size.height);
 }
 }
 
-  // effacement les tampons de couleur/profondeur
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+// clear the buffers
 
-  // dessin...
+glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+glLoadIdentity(); 
+gluPerspective(45.0f, 800.0f / 600.0f, 1.0f, 500.0f);
+// draw...
 
-  // termine la trame courante (en interne, échange les deux tampons de rendu)
-  window.display();
+glTranslatef(0.0f,0.0f,-5.0f);
+glRotatef(Rotate,1.0f,1.0f,1.0f);
+glEnable(GL_TEXTURE_2D);
+glBindTexture(GL_TEXTURE_2D, Texture);
+glColor4f(1.f, 1.f, 1.f, 1.f);
+
+glBegin(GL_QUADS);
+// Front Face
+glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);  // Bottom Left Of The Texture and Quad
+glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);  // Bottom Right Of The Texture and Quad
+glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);  // Top Right Of The Texture and Quad
+glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);  // Top Left Of The Texture and Quad
+// Back Face
+glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);  // Bottom Right Of The Texture and Quad
+glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);  // Top Right Of The Texture and Quad
+glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);  // Top Left Of The Texture and Quad
+glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);  // Bottom Left Of The Texture and Quad
+// Top Face
+glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);  // Top Left Of The Texture and Quad
+glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,  1.0f,  1.0f);  // Bottom Left Of The Texture and Quad
+glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f,  1.0f,  1.0f);  // Bottom Right Of The Texture and Quad
+glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);  // Top Right Of The Texture and Quad
+// Bottom Face
+glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);  // Top Right Of The Texture and Quad
+glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f, -1.0f, -1.0f);  // Top Left Of The Texture and Quad
+glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);  // Bottom Left Of The Texture and Quad
+glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);  // Bottom Right Of The Texture and Quad
+// Right face
+glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);  // Bottom Right Of The Texture and Quad
+glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);  // Top Right Of The Texture and Quad
+glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);  // Top Left Of The Texture and Quad
+glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);  // Bottom Left Of The Texture and Quad
+// Left Face
+glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);  // Bottom Left Of The Texture and Quad
+glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);  // Bottom Right Of The Texture and Quad
+glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);  // Top Right Of The Texture and Quad
+glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);  // Top Left Of The Texture and Quad
+glEnd();
+ 
+Rotate+=0.5f;
+
+
+// end the current frame (internally swaps the front and back buffers)
+window.display();
+ 
 }
 
-  // libération des ressources...
+// release resources...
 
-  return 0;
+
+return 0;
 }
