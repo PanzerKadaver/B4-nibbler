dcbc1f31c57f73295974f890db122da5ed8710c3
aubert_n <aubert_n@epitech.eu>
2014-04-05 16:01:29 +0200

    openGL 4 da newbs

--- openGLPourLesNuls/main.cpp
+++ openGLPourLesNuls/main.cpp
@@ -5,16 +5,50 @@
 // Login   <aubert_n@epitech.net>
 // 
 // Started on  Fri Apr  4 21:12:23 2014 Nathan AUBERT
-// Last update Fri Apr  4 22:11:19 2014 Nathan AUBERT
+// Last update Sat Apr  5 16:00:33 2014 Nathan AUBERT
 //
 
-#include <SFML/OpenGL.hpp>
 #include <SFML/Window.hpp>
-#include <SFML/System.hpp>
+#include <SFML/OpenGL.hpp>
 
 int main()
 {
-  sf::Window window(sf::VideoMode(800, 600), "OpenGL");
+  // crée la fenêtre
+  sf::Window window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Default, sf::ContextSettings(32));
+  window.setVerticalSyncEnabled(true);
 
-  glEnable(GL_TEXTURE_2D);
+  // chargement des ressources, initialisation des états OpenGL, ...
+
+  // la boucle principale
+  bool running = true;
+  while (running)
+    {
+  // gestion des évènements
+  sf::Event event;
+  while (window.pollEvent(event))
+    {
+  if (event.type == sf::Event::Closed)
+    {
+  // on stoppe le programme
+  running = false;
+}
+  else if (event.type == sf::Event::Resized)
+    {
+  // on ajuste le viewport lorsque la fenêtre est redimensionnée
+  glViewport(0, 0, event.size.width, event.size.height);
+}
+}
+
+  // effacement les tampons de couleur/profondeur
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  // dessin...
+
+  // termine la trame courante (en interne, échange les deux tampons de rendu)
+  window.display();
+}
+
+  // libération des ressources...
+
+  return 0;
 }
