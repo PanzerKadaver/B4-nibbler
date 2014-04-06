1275a76bcfef0a4def24cd0754fc287c11ec3332
aubert_n <aubert_n@epitech.eu>
2014-04-06 11:46:13 +0200

    sme magic

--- Nibbler5.0/lib_nibbler_SFML/SFMLCanvas.cpp
+++ Nibbler5.0/lib_nibbler_SFML/SFMLCanvas.cpp
@@ -1,9 +1,19 @@
-#ifdef Q_WS_X11
-#include <Qt/qx11info_x11.h>
-#include <X11/Xlib.h>
-#endif
+//
+// SFMLCanvas.cpp for  in /home/aubert_n/rep/nibbler/Nibbler5.0/lib_nibbler_SFML
+// 
+// Made by Nathan AUBERT
+// Login   <aubert_n@epitech.net>
+// 
+// Started on  Sun Apr  6 11:29:35 2014 Nathan AUBERT
+// Last update Sun Apr  6 11:44:22 2014 Nathan AUBERT
+//
 
 #include "SFMLCanvas.hpp"
+
+#ifdef Q_WS_X11
+#include <QtGui/QX11Info>
+#include <X11/Xlib.h>
+#endif
 
 SFMLCanvas::SFMLCanvas(QWidget &parent, const QPoint &pos, const QSize &size, uint framerate) :
   QWidget(&parent),
--- Nibbler5.0/lib_nibbler_SFML/SFMLCanvas.hpp
+++ Nibbler5.0/lib_nibbler_SFML/SFMLCanvas.hpp
@@ -1,3 +1,13 @@
+//
+// SFMLCanvas.hpp for  in /home/aubert_n/rep/nibbler/Nibbler5.0/lib_nibbler_SFML
+// 
+// Made by Nathan AUBERT
+// Login   <aubert_n@epitech.net>
+// 
+// Started on  Sun Apr  6 11:29:42 2014 Nathan AUBERT
+// Last update Sun Apr  6 11:29:43 2014 Nathan AUBERT
+//
+
 #ifndef NIBBLER_SFML_CANVAS_HPP_
 # define NIBBLER_SFML_CANVAS_HPP_
 
